6ead5e396d5a8bfb73dfbf2f01aebbd0afb4bef2
GavinCoupDigipen <53310510+GavinCoupDigipen@users.noreply.github.com>
2021-11-02 16:23:13 -0700

    Basic Enemies

---
+++ Assets/Scripts/Enemies/ProjectileEnemyAI.cs
@@ -0,0 +1,109 @@
+﻿//------------------------------------------------------------------------------
+//
+// File Name:	ProjectileEnemyAI.cs
+// Author(s):	Gavin Cooper (gavin.cooper@digipen.edu)
+// Project:	    RubyPlatypus
+// Course:	    WANIC VGP2
+//
+// Copyright © 2021 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileEnemyAI : MonoBehaviour
+{
+    [Tooltip("The health of the enemy")]
+    public float health = 1;
+    [Tooltip("The move speed of the enemy")]
+    public float moveSpeed = 2;
+    [Tooltip("How fast the enemy slows down")]
+    public float slowDownSpeed = 0.01f;
+    [Tooltip("The range at which the enemy will detect the player")]
+    public float detectionRange = 15;
+    [Tooltip("The farthest range the enemy will shot at")]
+    public float maxShootingRange = 10;
+    [Tooltip("The closest range the enemy will shot at")]
+    public float minShootingRange = 7;
+    [Tooltip("The move speed of the summoned projectile")]
+    public float projectileSpeed = 4;
+    [Tooltip("The time it takes to shoot a projectile")]
+    public float shootTime = 0.25f;
+    [Tooltip("The time before the projectile can be shot again")]
+    public float shootCooldownTime = 0.5f;
+    [Tooltip("The prefab of an object that will be the projectile")]
+    public GameObject projectilePrefab;
+
+    private Rigidbody2D enemyRB;
+    private SpriteRenderer enemySR;
+    private GameObject target;
+    private Vector2 dirrection;
+
+    private bool shooting = false;
+    private Color startingColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+        enemySR = GetComponent<SpriteRenderer>();
+        target = GameObject.FindGameObjectWithTag("Player");
+
+        startingColor = enemySR.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirrection = target.transform.position - transform.position;
+        if (shooting == false)
+        {
+            if (dirrection.magnitude <= minShootingRange)
+            {
+                dirrection = dirrection.normalized;
+                enemyRB.velocity = -dirrection * moveSpeed;
+            }
+            if (dirrection.magnitude <= maxShootingRange)
+            {
+                Invoke("ShootProjectile", shootTime);
+                shooting = true;
+            }
+            else if (dirrection.magnitude <= detectionRange)
+            {
+                dirrection = dirrection.normalized;
+                enemyRB.velocity = dirrection * moveSpeed;
+            }
+            else
+            {
+                enemyRB.velocity = Vector2.Lerp(enemyRB.velocity, new Vector2(0, 0), slowDownSpeed);
+            }
+        }
+        else
+        {
+            enemyRB.velocity = Vector2.Lerp(enemyRB.velocity, new Vector2(0, 0), slowDownSpeed);
+        }
+    }
+
+    // Check for collision with sword
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    // Summon the projecile, rotate it, and add force
+    private void ShootProjectile()
+    {
+        enemySR.color = startingColor;
+
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        float projectileRotation;
+        dirrection = (target.transform.position - transform.position).normalized;
+        projectileRotation = Mathf.Atan2(dirrection.x, dirrection.y) * Mathf.Rad2Deg;
+        projectile.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -projectileRotation));
+        projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * projectileSpeed;
+
+        shooting = false;
+    }
+}
