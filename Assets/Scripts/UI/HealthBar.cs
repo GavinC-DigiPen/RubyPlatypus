ba0d5f79e5da18137dc116f464f655c43e086127
GavinCoupDigipen <53310510+GavinCoupDigipen@users.noreply.github.com>
2021-11-03 16:22:40 -0700

    Added HealthBar

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,44 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	HealthBar.cs
+// Author(s):	Gavin Cooper (gavin.cooper@digipen.edu)
+// Project:	    RubyPlatypus
+// Course:	    WANIC VGP2
+//
+// Copyright © 2021 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [Tooltip("The game object that is the health bar")]
+    public GameObject healthBar;
+
+    private Health healthScript;
+    private Vector2 startingPosition;
+    private Vector2 startingScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = healthBar.transform.localPosition;
+        startingScale = healthBar.transform.localScale;
+        UpdateLives();
+        healthScript = FindObjectOfType<Health>();
+        Health.OnHealthChange.AddListener(UpdateLives);
+    }
+      
+    // Function that updates health bar and text
+    void UpdateLives()
+    {
+        float scaleScaler = (float)healthScript.currenthealth / healthScript.maxHealth;
+        float newXScale = startingScale.x * scaleScaler;
+
+        healthBar.transform.localScale = new Vector2(newXScale, startingScale.y);
+        healthBar.transform.localPosition = new Vector2(startingPosition.x - (startingScale.x - newXScale) / 2f, startingPosition.y);
+    }
+}
